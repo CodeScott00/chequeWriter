7422bc9ba315fac9ed66bec476e4ef37c23a99e8
CodeScott00 <dougal_00@hotmail.com>
2023-02-16 14:46:56 +1100

    capitalisation, commas, plurisation, larger numbers refactor and added original cheque_writer_with_decimal for reference

--- cheque_writer_with_decimal.js
+++ cheque_writer_with_decimal.js
@@ -1,6 +1,6 @@
 // Declaring some test variables
 const testZeros = [1, 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000]
-const testNums = [17, 782, 9321, 54621, 234506, 92738473, 999999999, 1000200]
+const testNums = [17, 782, 9321, 54621, 234506, 92738473, 999999999, 1000200, 56.23]
 
 function chequeWriter(moneyDecimal) {
     // create arrays of units, tens and scales to draw on later
@@ -8,9 +8,9 @@ function chequeWriter(moneyDecimal) {
     let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety',];
     let scales = ['', 'thousand', 'million',];
 
-    // change input number to string
-    let stringNum = moneyDecimal.toString()
-
+    // ***DECIMAL ADDITION*** change input number to string fixed to two decimal places
+    let stringNum = moneyDecimal.toFixed(2).toString()
+    // console.log(stringNum)
     // split string number into chunks from right to left in groups of threes
     let counter = stringNum.length;
     let chunks = [];
@@ -21,14 +21,39 @@ function chequeWriter(moneyDecimal) {
         // console.log("This loop sliced from number", counter, "to", end, "and added this to chunks", chunks) // DEBUG
     };
     // console.log("Chunks - array of string numbers", chunks) // DEBUG
-    
+
+    // ***DECIMAL ADDITION** Taking out the decimal point and separating ints & decimal into two separate arrays
+    let splitDecimal = chunks[0].split('.')
+    console.log("splitDecimalL: ", splitDecimal)
+    let pureDecimal = splitDecimal[splitDecimal.length - 1]
+    // console.log("pureDecimal: ", pureDecimal)
+    // chunks[0] = pureDecimal
+    // console.log("chunks[0]", chunks[0])
+    const decimalChunk = pureDecimal
+    console.log("decimalChunk",decimalChunk)
+    console.log("Chunks", chunks)
+    const intChunks = chunks.slice(1)
+    console.log("intChunks", intChunks)
+    // console.log("I'm the decimal", decimalChunk)
+    //console.log("I'm what used to be just chunks, now intChunks", intChunks)
+
+    // *** DECIMAL ADDITION ** trying to turn decimal to cents
+    let decimalChunkLength = decimalChunk.length;
+    for(let i = 0; i < decimalChunkLength; i++) {
+        let decArray = decimalChunk[i].split('').reverse().map(parseFloat);
+        console.log(decArray, "decArray")
+        // UP TO HERE
+        // pull logic from intChunks for loop to turn this to cents and add to end of intChunks return
+    }
+
+    // setting up for the first for loop
+    let intChunksLength = intChunks.length;
     let word = '';
     let words = [];
-    let chunksLength = chunks.length; 
-    // console.log("Number of Chunks in array", chunksLength); // DEBUG
-    for(let i = 0; i < chunksLength; i++) {
+    
+    for(let i = 0; i < intChunksLength; i++) {
         // Takes first element of chunks array, splits the string into individual elements, reverses their order and converts them to numbers in a new array
-        let intsArray = chunks[i].split('').reverse().map(parseFloat); 
+        let intsArray = intChunks[i].split('').reverse().map(parseFloat); 
         // console.log("BEFORE IF BLOCKS", 'intsArray', intsArray, "word", word, "words", words) // DEBUG
 
         // checks if number is in the teens and adds ten to first number so it can access correct element in unit array
